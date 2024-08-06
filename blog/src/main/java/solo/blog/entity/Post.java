3b75b46e913d1d1e221b1c41027aa0b71a18f05f
KaarfSub <rpdirwk1025@naver.com>
2024-08-06 15:33:55 +0900

    Dev - 포스트(글 작성) 엔티티

--- blog/src/main/java/solo/blog/entity/Post.java
+++ blog/src/main/java/solo/blog/entity/Post.java
@@ -1,4 +1,71 @@
 package solo.blog.entity;
 
+import java.time.LocalDateTime;
+
 public class Post {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private Long memberId;
+
+
+    public Post(Long id, String title, String content, LocalDateTime createdDate, Long memberId) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.createdDate = createdDate;
+        this.memberId = memberId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", content =" + content +
+                ", createdDate =" + createdDate +
+                ", memberId =" + memberId +
+                '}';
+    }
 }
