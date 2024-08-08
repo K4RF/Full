e0fd5464c70fd86506a2c55cd1b3287f74458986
KaarfSub <rpdirwk1025@naver.com>
2024-08-08 16:18:21 +0900

    edit - 파일명 수정

--- blog/src/main/java/solo/blog/controller/v2/post/BasicPostController.java
+++ blog/src/main/java/solo/blog/controller/v2/post/BasicPostController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import solo.blog.entity.v2.Post;
 import solo.blog.repository.v2.PostRepository;
@@ -12,7 +13,7 @@ import solo.blog.repository.v2.PostRepository;
 import java.util.List;
 
 @Controller
-@RequestMapping("/post/postList")
+@RequestMapping("/post/basic/postList")
 @RequiredArgsConstructor
 public class BasicPostController {
     private final PostRepository postRepository;
@@ -21,7 +22,14 @@ public class BasicPostController {
     public String post(Model model) {
         List<Post> posts = postRepository.findAll();
         model.addAttribute("posts", posts);
-        return "/post/basic/posts";
+        return "post/basic/postList";
+    }
+
+    @GetMapping("/{postId}")
+    public String post(@PathVariable long postId, Model model) {
+        Post post = postRepository.findById(postId);
+        model.addAttribute("post", post);
+        return "post/basic/post";
     }
 
     @PostConstruct
