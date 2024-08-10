049585948cbe97bc5a4c514da1f5bd61b6f7a880
K4RF <rpdirwk1025@naver.com>
2024-08-10 15:26:23 +0900

    Dev - 작성자를 회원 이름으로 자동 등록되도록

--- blog/src/main/java/solo/blog/config/AppConfig.java
+++ blog/src/main/java/solo/blog/config/AppConfig.java
@@ -3,14 +3,20 @@ package solo.blog.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import solo.blog.repository.v1.MemoryMemberRepositoryV1;
+import solo.blog.repository.v2.MemberRepository;
 import solo.blog.service.v1.MemberServiceV1;
 import solo.blog.service.v1.MemberServiceV1Impl;
 import solo.blog.service.v1.PostService;
 import solo.blog.service.v1.PostServiceImpl;
+import solo.blog.service.v2.MemberService;
 
 @Configuration
 public class AppConfig {
     @Bean
+    public MemberService memberService(MemberRepository memberRepository) {
+        return new MemberService(memberRepository);
+    }
+   // @Bean
     public MemberServiceV1 memberService(){
         return new MemberServiceV1Impl(memberRepository());
     }
