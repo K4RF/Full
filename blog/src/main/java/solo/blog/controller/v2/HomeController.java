0c509e41a2b2a62f4c15167654f1633818544793
K4RF <rpdirwk1025@naver.com>
2024-08-10 15:57:34 +0900

    Dev - 서블릿 HTTP 세션 로그인

--- blog/src/main/java/solo/blog/controller/v2/HomeController.java
+++ blog/src/main/java/solo/blog/controller/v2/HomeController.java
@@ -1,12 +1,14 @@
 package solo.blog.controller.v2;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import solo.blog.controller.session.SessionConst;
 import solo.blog.controller.session.SessionManager;
 import solo.blog.entity.v2.Member;
 import solo.blog.repository.v2.MemberRepository;
@@ -38,7 +40,7 @@ public class HomeController {
         return "loginHome";
     }
 
-    @GetMapping("/")
+    //@GetMapping("/")
     public String homeLoginSession(HttpServletRequest request, Model model) {
         // 세션 관리자에 저장된 회원 정보 조회
         Object member = (Member) sessionManager.getSession(request);
@@ -49,6 +51,27 @@ public class HomeController {
         }
 
         model.addAttribute("member", member);
+        return "loginHome";
+    }
+
+    @GetMapping("/")
+    public String homeLoginServlet(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession(false);
+        if (session == null) {
+            return "home";
+        }
+
+        // 세션 관리자에 저장된 회원 정보 조회
+        Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
+
+        // 로그인
+        // 세션에 회원 데이터가 없으면 home
+        if (loginMember == null) {
+            return "home";
+        }
+
+        // 세션이 유지되면 로그인으로 이동
+        model.addAttribute("member", loginMember);
         return "loginHome";
     }
 }
