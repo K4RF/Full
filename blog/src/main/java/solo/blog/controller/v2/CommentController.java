056d92adb87803a4e5e1e8ed1fde9392bbb77461
KaarfSub <rpdirwk1025@naver.com>
2024-08-13 14:06:50 +0900

    Ref - 포스트와 코멘트 컨트롤러 확실히 분리

--- blog/src/main/java/solo/blog/controller/v2/CommentController.java
+++ blog/src/main/java/solo/blog/controller/v2/CommentController.java
@@ -1,4 +1,4 @@
-package solo.blog.controller.v2.comment;
+package solo.blog.controller.v2;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import solo.blog.entity.v2.Comment;
 import solo.blog.service.v2.CommentService;
-import solo.blog.service.v2.MemberService;
+
 
 import java.util.List;
 
@@ -15,20 +15,17 @@ import java.util.List;
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
-    private final MemberService memberService;
-
     @GetMapping
     public String getComments(@PathVariable Long postId, Model model) {
         List<Comment> comments = commentService.getCommentsByPostId(postId);
         model.addAttribute("comments", comments);
         return "post/basic/post";
     }
-
-    @PostMapping("/add")
-    public String addComment(@PathVariable Long postId, @RequestParam String comet, Model model) {
-        Long memberId = 1L; // 여기에서 세션 또는 토큰에서 실제 로그인된 사용자 ID를 가져와야 합니다.
-        String author = memberService.findMember(memberId).getName();
+    @PostMapping
+    public String addComment(@PathVariable Long postId,
+                             @RequestParam String author,
+                             @RequestParam String comet) {
         commentService.addComment(postId, author, comet);
-        return "redirect:/post/basic/postList/{postId}";
+        return "redirect:/post/basic/postList/" + postId;
     }
 }
