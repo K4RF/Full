3bc3d30771ff9cfe44d7c802382b9a88a5566fa7
KaarfSub <rpdirwk1025@naver.com>
2024-08-29 16:06:17 +0900

    Test - 로그 예외

--- blog/src/test/java/solo/blog/service/jpa/tx/MemberTxServiceTest.java
+++ blog/src/test/java/solo/blog/service/jpa/tx/MemberTxServiceTest.java
@@ -8,6 +8,7 @@ import solo.blog.entity.database.tx.Member;
 import solo.blog.repository.jpa.tx.LogRepository;
 import solo.blog.repository.jpa.tx.MemberTxRepository;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
@@ -37,5 +38,25 @@ class MemberTxServiceTest {
         //then: 모든 데이터가 정상 저장된다.
         assertTrue(memberRepository.find(name).isPresent());
         assertTrue(logRepository.find(name).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional:OFF
+     * memberRepository @Transactional:ON
+     * logRepository    @Transactional:ON Exception
+     */
+    @Test
+    void outerTxOff_fail() {
+        //given
+        Member member = new Member("로그인", "로그예외_outerTxOff_fail", "비밀번호");
+        String name = member.getName();
+
+        //when
+        assertThatThrownBy(() -> memberService.joinV1(member))
+                .isInstanceOf(RuntimeException.class);
+
+        //when: log 데이터는 롤백된다.
+        assertTrue(memberRepository.find(name).isPresent());
+        assertTrue(logRepository.find(name).isEmpty());
     }
 }
