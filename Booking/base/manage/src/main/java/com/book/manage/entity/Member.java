1523acdbccbc02fda156999b34c8491ffd563015
KaarfSub <rpdirwk1025@naver.com>
2024-11-12 15:09:59 +0900

    add - 회원 가입 폼 작성, 회원에 검증 조건 추가(이름, 비밀 번호 자릿수 특수문자 검증)

--- Booking/base/manage/src/main/java/com/book/manage/entity/Member.java
+++ Booking/base/manage/src/main/java/com/book/manage/entity/Member.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -13,10 +16,16 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
+    @NotBlank(message = "ID는 필수입니다.")
+    @Pattern(regexp = "^[a-zA-Z0-9]{1,16}$", message = "ID는 영문과 숫자만 사용하며 1~16자 이내여야 합니다.")
     private String loginId;
 
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    @Size(min = 8, max = 16, message = "비밀번호는 8~16자 이내여야 합니다.")
     private String password;
 
+    @NotBlank(message = "닉네임은 필수입니다.")
+    @Pattern(regexp = "^[a-zA-Z가-힣0-9 ]+$", message = "닉네임에는 특수 문자를 사용할 수 없습니다.")
     private String nickname;
 
     public Member(){
