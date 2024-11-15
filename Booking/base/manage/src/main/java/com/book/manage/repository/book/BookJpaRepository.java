6e742a4740279fce00559706d1cc3f92542fc7f4
KaarfSub <rpdirwk1025@naver.com>
2024-11-15 15:01:09 +0900

    add - 책 리포지토리 생성

--- Booking/base/manage/src/main/java/com/book/manage/repository/book/BookJpaRepository.java
+++ Booking/base/manage/src/main/java/com/book/manage/repository/book/BookJpaRepository.java
@@ -1,18 +1,33 @@
 package com.book.manage.repository.book;
 
 import com.book.manage.entity.Book;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Slf4j
+@Repository
 public class BookJpaRepository implements BookRepository{
-    private final Entity
-    @Override
-    public Book save(Book book) {
-        return null;
+    private final EntityManager em;
+    private final JPAQueryFactory factory;
+
+    public BookJpaRepository(EntityManager em) {
+        this.em = em;
+        this.factory = new JPAQueryFactory(em);
     }
 
     @Override
-    public Optional<Book> findById(Long id) {
-        return Optional.empty();
+    public Book save(Book book) {
+        em.persist(book);
+        return book;
+    }
+
+    @Override
+    public Optional<Book> findById(Long bookId) {
+        Book book = em.find(Book.class, bookId);
+        return Optional.ofNullable(book);
     }
 }
--- Booking/base/manage/src/main/java/com/book/manage/repository/book/BookRepository.java
+++ Booking/base/manage/src/main/java/com/book/manage/repository/book/BookRepository.java
@@ -7,5 +7,5 @@ import java.util.Optional;
 
 public interface BookRepository {
     Book save(Book book);
-    Optional<Book> findById(Long id);
+    Optional<Book> findById(Long bookId);
 }
