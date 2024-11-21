d60d00d3fcfa0f42fd1a4b1cb5cac1c6d98f0e7d
K4RF <korea2han234@gmail.com>
2024-11-21 21:52:06 +0900

    fix - 회원 엔티티의 비밀번호 사이즈 제약 조건 해제

--- Book_ManageSys/base/manage/src/main/java/com/book/manage/entity/Member.java
+++ Book_ManageSys/base/manage/src/main/java/com/book/manage/entity/Member.java
@@ -21,7 +21,6 @@ public class Member {
     private String loginId;
 
     @NotBlank(message = "비밀번호는 필수입니다.")
-    @Size(min = 8, max = 16, message = "비밀번호는 8~16자 이내여야 합니다.")
     private String password;
 
     @NotBlank(message = "닉네임은 필수입니다.")
