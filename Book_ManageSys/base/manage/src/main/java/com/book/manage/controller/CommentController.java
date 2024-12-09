6ff717780520e3ce276872d7a9c66fbd0870d270
KaarfSub <rpdirwk1025@naver.com>
2024-12-09 16:19:31 +0900

    add - 댓글 컨트롤러 생성

--- Book_ManageSys/base/manage/src/main/java/com/book/manage/controller/CommentController.java
+++ Book_ManageSys/base/manage/src/main/java/com/book/manage/controller/CommentController.java
@@ -1,13 +1,12 @@
 package com.book.manage.controller;
 
 import com.book.manage.entity.Comment;
+import com.book.manage.entity.Member;
 import com.book.manage.service.book.comment.CommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,5 +21,42 @@ public class CommentController {
         List<Comment> comments = commentService.getCommentsByBookId(bookId);
         model.addAttribute("comments", comments);
         return "book/bookInfo";
+    }
+
+    @PostMapping
+    public String addComment(@PathVariable Long bookId,
+                             @RequestParam String review,
+                             @SessionAttribute(value = "loginMember", required = false) Member loginMember) {
+        // 로그인 여부 확인
+        if (loginMember == null) {
+            return "redirect:/login?redirectURL=/bookList/" + bookId;
+        }
+        //댓글 작성
+        commentService.addComment(bookId, loginMember.getNickname(), review);
+        return "redirect:/bookList" + bookId;
+    }
+
+    @PostMapping("/{commentId}/edit")
+    public String editComment(@PathVariable Long bookId,
+                              @PathVariable Long commentId,
+                              @RequestParam String newReview,
+                              @SessionAttribute(value = "loginMember", required = false) Member loginMember) {
+        if (loginMember == null) {
+            return "redirect:/login?redirectURL=/bookList/" + bookId;
+        }
+        commentService.updateComment(commentId, newReview, loginMember.getNickname());
+        return "redirect:/bookList" + bookId;
+    }
+
+    @PostMapping("/{commentId}/delete")
+    public String deleteComment(@PathVariable Long bookId,
+                                @PathVariable Long commentId,
+                                @SessionAttribute(value = "loginMember", required = false) Member loginMember) {
+        if (loginMember == null) {
+            return "redirect:/login?redirectURL=/bookList/" + bookId;
+        }
+        // 댓글 삭제 로직 실행
+        commentService.deleteComment(commentId, loginMember.getNickname());
+        return "redirect:/bookList" + bookId;
     }
 }
