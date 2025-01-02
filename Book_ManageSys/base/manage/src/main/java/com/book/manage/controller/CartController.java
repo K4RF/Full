d51d9ee48359ae4431bce4e1e56d9ccdf6eb9235
KaarfSub <rpdirwk1025@naver.com>
2025-01-02 09:47:08 +0900

    add - 장바구니 및 웹 폼

--- Book_ManageSys/base/manage/src/main/java/com/book/manage/controller/CartController.java
+++ Book_ManageSys/base/manage/src/main/java/com/book/manage/controller/CartController.java
@@ -2,7 +2,9 @@ package com.book.manage.controller;
 
 import com.book.manage.entity.Book;
 import com.book.manage.entity.Cart;
+import com.book.manage.entity.Member;
 import com.book.manage.service.book.BookService;
+import com.book.manage.service.order.OrderService;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -19,15 +21,17 @@ import java.util.Optional;
 public class CartController {
 
     private final BookService bookService;
+    private final OrderService orderService;
 
     @GetMapping
-    public String viewCart(@SessionAttribute(value = "cart", required = false) List<Cart> cart, Model model) {
+    public String viewCart(HttpSession session, @SessionAttribute(value = "cart", required = false) List<Cart> cart, Model model) {
         if (cart == null) {
             cart = new ArrayList<>();
+            session.setAttribute("cart", cart);
         }
         model.addAttribute("cart", cart);
         model.addAttribute("totalPrice", cart.stream().mapToInt(Cart::getTotalPrice).sum());
-        return "order/cartView"; // 장바구니 페이지
+        return "order/cartForm"; // 장바구니 페이지
     }
 
     @PostMapping("/{bookId}/add")
@@ -61,7 +65,7 @@ public class CartController {
             cart.add(newItem);
         }
 
-        return "redirect:/cart";
+        return "redirect:/bookList/" + bookId;
     }
 
     @PostMapping("/{bookId}/remove")
@@ -79,4 +83,20 @@ public class CartController {
         session.removeAttribute("cart");
         return "redirect:/cart";
     }
+
+    @PostMapping("/checkout")
+    public String checkout(
+            @SessionAttribute(value = "cart", required = false) List<Cart> cart,
+            @SessionAttribute(value = "loginMember", required = false) Member loginMember,
+            HttpSession session) {
+        if (loginMember == null) {
+            return "redirect:/login?redirectURL=/cart";
+        }
+        if (cart != null && !cart.isEmpty()) {
+            cart.forEach(item -> orderService.createOrder(loginMember.getMemberId(), item.getBookId(), item.getQuantity()));
+            session.removeAttribute("cart"); // 장바구니 비우기
+        }
+        return "redirect:/orderList";
+    }
+
 }
